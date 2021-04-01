0bd8c195adda8ca91cec7686c5c45fb40e1071ce
ahlersml <70450921+ahlersml@users.noreply.github.com>
2021-04-01 11:42:10 -0400

    fixed errors

---
+++ FinalProject/Migrations/20210401154156_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Info",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true),
+                    CollegeProgram = table.Column<string>(nullable: true),
+                    YearInProgram = table.Column<string>(nullable: true),
+                    Birthdate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Info", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Info");
+        }
+    }
+}
