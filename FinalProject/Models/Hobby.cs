7c687243f5796e3945eb0fbf3e887529e259466a
Josh Anness <annessjosh@gmail.com>
2021-04-05 13:22:06 -0400

    Added Hobby.cs

---
+++ FinalProject/Models/Hobby.cs
@@ -0,0 +1,12 @@
+namespace FinalProject.Models
+{
+    public class HobbyContext
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string FavoriteHobby { get; set; }
+        public string SecondFavoriteHobby { get; set; }
+        public string WhyYouLikeThem { get; set; }
+
+    }
+}
