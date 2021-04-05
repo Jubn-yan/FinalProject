d929c5fb84d5c3d6d0b90ef7d2f023e07a387787
Josh Anness <annessjosh@gmail.com>
2021-04-05 13:28:03 -0400

    Added HobbiesController.cs

---
+++ FinalProject/Controllers/HobbiesController.cs
@@ -0,0 +1,19 @@
+using FinalProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FinalProject.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class HobbiesController : ControllerBase
+    {
+        private readonly ILogger<HobbiesController> _logger;
+        private readonly HobbyContext _ctx;
+        public HobbiesController(ILogger<HobbiesController> logger, HobbyContext ctx)
+        {
+            _logger = logger;
+            _ctx = ctx;
+        }
+    }
+}
