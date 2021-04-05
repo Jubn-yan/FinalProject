d2b0f7319c61e24ca4c41ec1f0fad774e9683d9a
Josh Anness <annessjosh@gmail.com>
2021-04-05 14:31:17 -0400

    Added Hobby Migration

---
+++ FinalProject/Migrations/Hobby/20210405182620_Intial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Migrations.Hobby
+{
+    public partial class Intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hobbies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true),
+                    FavoriteHobby = table.Column<string>(nullable: true),
+                    SecondFavoriteHobby = table.Column<string>(nullable: true),
+                    WhyYouLikeThem = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hobbies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hobbies");
+        }
+    }
+}
