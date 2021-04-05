8f62e392ab1f58d587ce46ad18212a8bb5989a87
Josh Anness <annessjosh@gmail.com>
2021-04-05 15:44:33 -0400

    Added HobbyContext Info

--- FinalProject/Data/HobbyContext.cs
+++ FinalProject/Data/HobbyContext.cs
@@ -12,5 +12,27 @@ namespace FinalProject.Data
         }
 
         public DbSet<Hobby> Hobbies { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Hobby>().HasData(
+                new Hobby
+                {
+                    ID = 1,
+                    FullName = "Michael Ahlers",
+                    FavoriteHobby = "Basketball",
+                    SecondFavoriteHobby = "Baseball",
+                    WhyYouLikeThem = "I like staying active"
+                },
+                new Hobby
+                {
+                    ID = 2,
+                    FullName = "Josh Anness",
+                    FavoriteHobby = "Fishing",
+                    SecondFavoriteHobby = "Basketball",
+                    WhyYouLikeThem = "I like being outside and staying active"
+                }
+            );
+        }
     }
 }
